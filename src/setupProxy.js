5aa0de1e2cc9bc567d80fcdc6bfcea73ee1e623b
DA-PC-0473\admin <covj12@gmail.com>
2021-07-01 13:09:09 +0900

    테스트용 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// cors 도메인 방지를 위한 중계서버 
-// module.exports = function (app) {
-//     app.use(
-//         createProxyMiddleware('/containers', {
-//             target : 'http://localhost:2375',
-//             changeOrigin: true
-//         })
-//     );
-// }
+//cors 도메인 방지를 위한 중개서버 
+module.exports = function (app) {
+    app.use(
+        createProxyMiddleware('/containers', {
+            target : '*',
+            changeOrigin: true
+        })
+    );
+}
