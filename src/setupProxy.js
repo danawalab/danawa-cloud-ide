897afd281edfd0a2642171355c554a3401897551
DA-PC-0473\admin <covj12@gmail.com>
2021-07-01 14:46:22 +0900

    4000번 프록시 추가

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,16 +2,16 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // cors 도메인 방지를 위한 중계서버 
 module.exports = function (app) {
-    // app.use(
-    //     createProxyMiddleware('/containers', {
-    //         target : 'http://localhost:2375',
-    //         changeOrigin: true
-    //     })
-    // );
-    // app.use(
-    //     createProxyMiddleware('/', {
-    //         target : 'http://es2.danawa.io:3000',
-    //         changeOrigin: true
-    //     })
-    // );
+    app.use(
+        createProxyMiddleware('/containers', {
+            target : 'http://localhost:2375',
+            changeOrigin: true
+        })
+    );
+    app.use(
+        createProxyMiddleware('/api/join', {
+            target : 'http://localhost:4000',
+            changeOrigin: true
+        })
+    );
 }
