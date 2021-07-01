e956af2fcddd940ebaf1b00efa7eeacb3a09adb2
DA-PC-0473\admin <covj12@gmail.com>
2021-07-01 13:45:16 +0900

    프록시 서버 수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,17 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-//cors 도메인 방지를 위한 중개서버 
+// cors 도메인 방지를 위한 중계서버 
 module.exports = function (app) {
-    app.use(
-        createProxyMiddleware('/containers', {
-            target : 'http://localhost:2375',
-            changeOrigin: true,
-            secure: false
-        })
-    );
+    // app.use(
+    //     createProxyMiddleware('/containers', {
+    //         target : 'http://localhost:2375',
+    //         changeOrigin: true
+    //     })
+    // );
+//     app.use(
+//         createProxyMiddleware('/', {
+//             target : 'http://es2.danawa.io:3000',
+//             changeOrigin: true
+//         })
+//     );
 }
