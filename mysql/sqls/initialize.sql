dee91373d4f8b2669bcd730df89200cfd66d98cf
DA-PC-0473\admin <covj12@gmail.com>
2021-07-30 13:24:26 +0900

    테이블 정보 수정

--- mysql/sqls/initialize.sql
+++ mysql/sqls/initialize.sql
@@ -6,16 +6,13 @@ USE react;
 create table CONTAINER_INFO 
   (
   	user_id varchar(32) not null,
-  	container_id varchar(32) not null primary key,
+  	container_id varchar(100) not null primary key,
 	container_nm varchar(32) not null,
 	note_txt varchar(500) not null,
-	region_cd varchar(32) not null,
-	tmlt_cd varchar(32) not null,
 	tmlt_dtl varchar(32) not null,
 	stack_cd varchar(32) not null,
 	addpkg_cd_1 varchar(32) not null,
-	addpkg_cd_2 varchar(32) not null,
-	addpkg_cd_3 varchar(32) not null,
+    port varchar(32) not null,
   	update_dts datetime,
  	insert_dts datetime
   );
@@ -23,7 +20,8 @@ create table CONTAINER_INFO
 create table USER_INFO 
 (
 	user_id varchar(32) not null primary key,
-	user_pwd varchar(32) not null,
+	user_pwd varchar(200) not null,
+	serialkey varchar(200),
 	update_dts datetime,
     insert_dts datetime
 );
